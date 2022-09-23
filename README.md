c427a87258d0e91ad8e445b776c08bdcde2bd5ad
oikawa <oikawa@MasakinoMacBook-Pro.local>
2022-09-24 01:46:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## references
+
+[1] Shin-sibainu/react-recoil-tutorial
+https://github.com/Shin-sibainu/react-recoil-tutorial
---
+++ src/components/InputTask.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import './inputTask.css';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { inputTitleState } from '../states/inputTitleState';
+import { addTitleState } from '../states/addTitleState';
+
+const getKey = () => Math.random().toString(32).substring(2);
+
+const InputTask = () => {
+  const [inputTitle, setInputTitle] = useRecoilState(inputTitleState);
+  const [addTitle, setAddTitle] = useRecoilState(addTitleState);
+
+  const handleClick = () => {
+    setAddTitle([...addTitle, { id: getKey(), title: inputTitle }]);
+    setInputTitle('');
+    console.log(addTitle);
+  };
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputTitle(e.target.value);
+      console.log(inputTitle);
+    },
+    [inputTitle]
+  );
+
+  return (
+    <div className="inputField">
+      <input type="text" className="inputTitle" onChange={onChange} value={inputTitle} />
+      <button type="button" className="addButton" onClick={handleClick}>
+        追加
+      </button>
+    </div>
+  );
+};
+
+export default InputTask;
